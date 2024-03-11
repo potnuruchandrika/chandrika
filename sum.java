5c11646cd3709f274728a97b9209d8493b73d1a5
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-03-11 16:10:31 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,21 @@
+package java;
+
+public class sum {
+	
+	int add(int a,int b)
+	{
+		int res=a+b;
+		return res;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+        sum ob=new sum();
+        System.out.println(ob.add(3, 4));
+        int x=ob.add(3, 4);
+        System.out.println(x);
+        
+       
+	}
+
+}
