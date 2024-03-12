8bd7a3f176308edc2cb12d61f4bb998471ba8cc7
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-03-12 16:26:50 +0530

    Add files via upload

---
+++ method2.java
@@ -0,0 +1,18 @@
+
+public class method2 {
+	int sum(int a, int b) {
+		int c=a+b;
+		return c;
+		//System.out.println(c);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method2 ob=new method2();
+				int a=ob.sum(4,6);
+				System.out.println(a );
+
+	}
+
+}
