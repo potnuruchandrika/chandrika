2971e24a4c6cfaf3627a6fd02a511c1f84c55d9b
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-03-12 16:27:51 +0530

    Add files via upload

---
+++ method1.java
@@ -0,0 +1,18 @@
+package methods;
+
+public class method1 {
+	
+	void sum(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method1 ob=new method1();
+		ob.sum(6,9);
+		
+
+	}
+
+}
---
+++ method3.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class method3 {
+	public static void add(int a, int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method3.add(6,7);
+
+	}
+
+}
