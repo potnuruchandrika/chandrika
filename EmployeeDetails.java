9b1c087d494a4fb9f636e6087a9cd87dd770a62a
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-04-18 15:01:12 +0530

    project1

---
+++ EmployeeDetails.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class EmployeeDetails {
+	int eid;
+	String ename;
+	public EmployeeDetails (int eid, String ename) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		
+	}
+	public int getId() {
+		return eid;
+	}
+	public void setId(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return ename;
+	}
+	public void setName(String name) {
+		this.ename = ename;
+	}
+	
+	
+	}
+	
+
+		 
+
+
