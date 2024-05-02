82c2a46753b10f406f30caf59c8ef76855b3e1fa
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-05-02 16:53:39 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,22 @@
+package paymentapp.com;
+
+public class User {
+	
+	String username;
+	String Password;
+	public String getUserName() {
+		return username;
+	}
+	public void setUserName(String UserName) {
+		UserName = UserName;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	
+}
+
