88c100259406f7ff36780446461b586dc8d6617c
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-05-02 16:53:07 +0530

    Add files via upload

---
+++ Loginservlet.java
@@ -0,0 +1,34 @@
+package paymentapp.com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class Loginservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Loginservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		User u=new User();
+		u.setUserName(username);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
